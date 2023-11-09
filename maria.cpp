88d7fd7014448396804c3e5a3724577b4bc921c3
sofivasa <sofivasa@myuax.com>
2023-11-09 09:43:07 +0100

    subida1

---
+++ maria.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Sofia D.C Vazquez Sanchez on 09/11/23.
+//
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+
+
+    cout << "Introduzca el primer numero: ";
+    cin >> a;
+    cout << "Introduzca el segundo numero: ";
+    cin >> b;
+    int suma=a+b;
+    cout << "La suma de los dos numeros es: " << suma << endl;
+return 0;
+}
